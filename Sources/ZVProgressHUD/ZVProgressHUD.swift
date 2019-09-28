a9c33100fbdc69e2878ec0563bb24c0720feeb86
zevwings <zev.wings@gmail.com>
2019-09-28 11:52:50 +0800

    remove unuse sources

--- Sources/ZVProgressHUD/ZVProgressHUD.swift
+++
@@ -1,3 +0,0 @@
-struct ZVProgressHUD {
-    var text = "Hello, World!"
-}
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import ZVProgressHUDTests
-
-var tests = [XCTestCaseEntry]()
-tests += ZVProgressHUDTests.allTests()
-XCTMain(tests)
