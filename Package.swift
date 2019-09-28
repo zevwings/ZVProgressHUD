4644d3d598cb542da5af9f0e1158f856acfb274f
zevwings <zev.wings@gmail.com>
2019-09-28 11:55:35 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -7,18 +7,14 @@ let package = Package(
     name: "ZVProgressHUD",
     platforms: [.iOS(.v8)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ZVProgressHUD",
             targets: ["ZVProgressHUD"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/zevwings/ZVActivityIndicatorView.git", from: "0.2.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ZVProgressHUD",
             dependencies: ["ZVActivityIndicatorView"],
