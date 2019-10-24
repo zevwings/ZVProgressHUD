3159da8978bcd060dbf5f8f295e7009734ffa08c
zevwings <zev.wings@gmail.com>
2019-10-24 10:38:03 +0800

    fix spm in xcode 11

--- ZVProgressHUD/ProgressHUD+Operations.swift
+++ ZVProgressHUD/ProgressHUD+Operations.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 zevwings. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 // MARK: - Handle
 
--- ZVProgressHUD/ZVProgressHUD.h
+++ ZVProgressHUD/ZVProgressHUD.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017-2019 zevwings. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ZVProgressHUD.
 FOUNDATION_EXPORT double ZVProgressHUDVersionNumber;
