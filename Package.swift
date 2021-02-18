7e2adfa6184c999c3acdf1251c5e29ed69fab497
zevwings <zev.wings@gmail.com>
2021-02-18 09:22:18 +0800

    update podspec

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ZVProgressHUD",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(
             name: "ZVProgressHUD",
